b2e1b3e42d5ce09b7d5c30f907766329f0d0dc6d
Samuel Scozzari <s_scozzari@libero.it>
2022-03-08 17:09:30 +0100

    modifies file structure

--- resources/views/partials/footernav.blade.php
+++ resources/views/partials/footernav.blade.php
@@ -1,23 +1,6 @@
 {{-- End Section --}}
 <div class="spam-cont">
-  <input type="button" value="ADD NEW COMICS" />
-  <nav>
-    <ul>
-      <li>
-        <img src="{{ asset('img/footer-facebook.png') }}" alt="" />
-      </li>
-      <li>
-        <img src="{{ asset('img/footer-twitter.png') }}" alt="" />
-      </li>
-      <li>
-        <img src="{{ asset('img/footer-youtube.png') }}" alt="" />
-      </li>
-      <li>
-        <img src="{{ asset('img/footer-pinterest.png') }}" alt="" />
-      </li>
-      <li>
-        <img src="{{ asset('img/footer-periscope.png') }}" alt="" />
-      </li>
-    </ul>
-  </nav>
+  <a href="{{ route('comics.create') }}">
+    <input type="button" value="ADD NEW COMICS" />
+  </a>
 </div>
