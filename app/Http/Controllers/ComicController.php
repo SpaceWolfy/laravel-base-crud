cabd8880b9f917e6f407fe5dcca9b289959e3b81
Samuel Scozzari <s_scozzari@libero.it>
2022-03-08 16:17:02 +0100

    adds db callback

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,7 +14,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        return view("comics.index");
+        $data = Comic::all();
+        return view("comics.index", compact('data'));
     }
 
     /**
