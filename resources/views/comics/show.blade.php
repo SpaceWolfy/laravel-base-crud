bd30587f48f5833d690aee823b8787f9ecc1efc5
Samuel Scozzari <s_scozzari@libero.it>
2022-03-09 14:49:52 +0100

    includes delete button partial file

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -11,9 +11,13 @@
         <div><strong>Tipologia :</strong> {{ $comic->type }}</div>
         <div><strong>Descrizione :</strong> {{ $comic->description }}</div>
 
-        <a href="{{ route('comics.edit', $comic->id) }}">
-          <button class="button" type="submit">Modifica le informazioni</button>
-        </a>
+        <div class="btn-area">
+          <a href="{{ route('comics.edit', $comic->id) }}">
+            <button class="button" type="submit">Modifica le informazioni</button>
+          </a>
+
+          @include('partials.btnDel', ['id' => $comic->id, 'route' => 'comics.destroy'])
+        </div>
     </div>
 
     <div class="jumbo-thumb">
